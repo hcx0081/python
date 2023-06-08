d53bff24ec56cb28cd489bf7a0eef1cfaa2b71da
hcx0081 <623363564@qq.com>
2023-06-08 18:03:19 +0800

    python

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ print(True + 1)  # 2
 print(type(range(1, 2)))
 
 print(dict({1: 1, 2: 2, 3: 3}))
+
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+print(enumerate(seasons))  # <enumerate object at 0x000001714A844380>
+print(list(enumerate(seasons)))  # [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
