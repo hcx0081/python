39f69c9295486fd047d366f2e17e3e83b2340949
hcx0081 <623363564@qq.com>
2023-06-08 08:51:38 +0800

    python

--- clz.py
+++ clz.py
@@ -1,10 +1,4 @@
-def say():
-    print("hello")
-
-
 class Person:
-    __age = 20
-
     @classmethod
     def cm(self):
         print("类方法")
@@ -15,23 +9,3 @@ class Person:
 
     def im(self):
         print("实例方法")
-
-    def __init__(self, name, say):
-        self.name = name
-        self.say = say
-
-
-Person.im("zhangsan")
-p = Person("张三", say)
-print(p.name)  # 张三
-p.say()  # hello
-
-# p = Person()
-# p.name = "张三"
-# print(p.name)  # 张三
-# p.say = say
-# p.say()  # hello
-
-print(dir(Person))
-print(dir(p))
-print(dir())
